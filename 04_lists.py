9174250a491ca16ad2e86185eb357cec7431526e
CodeDelka <delkadrysadel@gmail.com>
2025-02-05 22:52:52 -0500

    04_lists_comprehensionsv2

--- 04_lists.py
+++ 04_lists.py
@@ -1,4 +1,4 @@
-##agregar elementos a una lista o array
+'''##agregar elementos a una lista o array
 numbers = []
 for element in range(1,11):
     numbers.append(element * 2)
@@ -6,4 +6,15 @@ print(numbers)
 
 ##agregar elementos a una lista de manera comprimida
 numbers_v2 = [element * 2 for element in range (1,11)]
+print(numbers_v2)'''
+
+###listas con condicional
+numbers = []
+for i in range(1,11):
+ if i % 2 == 0:
+    numbers.append(i * 2)
+print(numbers)
+
+###listas con condicional (comprehesion)
+numbers_v2 = [i * 2 for i in range (1,11) if i % 2 == 0]
 print(numbers_v2)
