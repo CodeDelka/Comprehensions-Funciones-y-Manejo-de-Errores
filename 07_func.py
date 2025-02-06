091c214badbaa6f5225d00a6f58be50e0ae5e7b1
CodeDelka <delkadrysadel@gmail.com>
2025-02-05 23:35:49 -0500

    07_funciones

---
+++ 07_func.py
@@ -0,0 +1,10 @@
+def my_print(text):
+    print(text * 2)
+my_print('este es mi texto ')       
+my_print('hola')
+def suma(a,b):
+   my_print(a+b)
+
+suma(154654,245646)    
+suma(21332132123,56465456)    
+    
